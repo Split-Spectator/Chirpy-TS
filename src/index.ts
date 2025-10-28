06f73ca20f8a9a9caa840c461e906e32eb147680
Split-Spectator <aosc9vma@mailer.me>
2025-10-28 16:33:50 -0400

    moved endpoints to API

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 import {handlerReadiness}  from "./handlers/health.js";
 import {middlewareLogResponses} from "./app/log.js";
 import {middlewareMetricsInc} from "./app/middleMetrics.js";
-import { handlerMetrics } from "./app/metrics/handlerMetrics.js";
-import {handlerReset} from "./app/reset/reset.js";
+import { handlerMetrics } from "./app/api/handlerMetrics.js";
+import {handlerReset} from "./app/api/reset.js";
 
 const app = express();
 const PORT = 8080;
@@ -12,9 +12,9 @@ const PORT = 8080;
 
 app.use(middlewareLogResponses);
 
-app.get("/metrics", handlerMetrics);
-app.get("/healthz", handlerReadiness);
-app.get("/reset", handlerReset);
+app.get("/api/metrics", handlerMetrics);
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/reset", handlerReset);
 app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 
