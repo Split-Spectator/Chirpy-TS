786381101f0126cbd0ff44f1592d1b5fcd561e44
Split-Spectator <aosc9vma@mailer.me>
2025-10-30 19:28:48 -0400

    Set up postgres

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
+process.loadEnvFile()
+ 
  type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
   };
   
+ 
+  export function getEnv(key: string): string {
+    const val = process.env[key];
+    if (!val) throw new Error(`Missing environment variable ${key}`);
+    return val;
+  }
+ 
   export const config: APIConfig = {
     fileserverHits: 0,
+    dbURL:  getEnv("DATABASE_URL)"),
   };
